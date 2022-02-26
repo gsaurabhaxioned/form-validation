0daa0179694862b2de5bbf9fd306ecbec46770f4
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-26 11:15:59 +0530

    form validation in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,30 @@ $(document).ready(function(){
     });
 });
 
+let firstname = document.querySelector('.firstname').value,
+lastname = document.querySelector('.lastname').value,
+position = document.querySelector('.position').value,
+company = document.querySelector('.company').value,
+company_type = document.querySelector('.company-type'),
+country = document.querySelector('.country'),
+email = document.querySelector('.email').value,
+check = document.querySelectorAll('input[type="checkbox"]'),
+firstname_pattern = /^[A-Za-z]{1,20}$/,
+lastname_pattern = /^[A-Za-z]{1,20}$/,
+position_pattern = /^[A-Za-z]{1,20}$/,
+company_pattern = /^[A-Za-z0-9]{1,20}$/,
+email_pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+inputs = document.querySelectorAll('input');
+
+inputs.forEach(i => {
+    i.addEventListener('blur',function(){
+        if(firstname === ""){
+            
+        }
+    })
+})
+
+
 
 
 
