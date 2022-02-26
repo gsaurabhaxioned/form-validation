84095649475b195e12174ef8c676eb6cd4fb56c9
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-26 15:07:15 +0530

    js duplicate code optimized

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,18 @@ $(document).ready(function () {
     });
 });
 
-let inputs = document.querySelectorAll('input'),
-    submit = document.querySelector('.submit'),
-    firstnameerror = document.querySelector('.firstnameerror'),
+    let firstnameerror = document.querySelector('.firstnameerror'),
     lastnameerror = document.querySelector('.lastnameerror'),
     positionerror = document.querySelector('.positionerror'),
     companyerror = document.querySelector('.companyerror'),
     companytypeerror = document.querySelector('.companytypeerror'),
     countryerror = document.querySelector('.countryerror'),
     emailerror = document.querySelector('.emailerror'),
-    checkboxerror = document.querySelector('.checkboxerror');
+    checkboxerror = document.querySelector('.checkboxerror'),
+    name_pattern = /^[A-Za-z]{1,20}$/,
+    position_pattern = /^[A-Za-z]{1,20}$/,
+    company_pattern = /^[A-Za-z0-9]{1,20}$/,
+    email_pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 function validate(e) {
     e.preventDefault();
@@ -25,12 +27,8 @@ function validate(e) {
         company_type = document.querySelector('.company-type'),
         country = document.querySelector('.country'),
         email = document.querySelector('.email').value,
-        checked_boxes = document.querySelectorAll('input[type="checkbox"]:checked'),
-        firstname_pattern = /^[A-Za-z]{1,20}$/,
-        lastname_pattern = /^[A-Za-z]{1,20}$/,
-        position_pattern = /^[A-Za-z]{1,20}$/,
-        company_pattern = /^[A-Za-z0-9]{1,20}$/,
-        email_pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        checked_boxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  
     firstnameerror.innerText = "";
     lastnameerror.innerText = "";
     positionerror.innerText = "";
@@ -42,16 +40,16 @@ function validate(e) {
 
     if (firstname === "") {
         firstnameerror.innerText = "please enter firstname";
-    } else if (!firstname.match(firstname_pattern)) {
+    } else if (!firstname.match(name_pattern)) {
         firstnameerror.innerText = "name can include alphabates only and characters between 1 to 20";
     } else if (lastname === "") {
         lastnameerror.innerText = "please enter lastname";
-    } else if (!lastname.match(lastname_pattern)) {
+    } else if (!lastname.match(name_pattern)) {
         lastnameerror.innerText = "name can include alphabates only and characters between 1 to 20";
     } else if (position === "") {
         positionerror.innerText = "please enter position";
     } else if (!position.match(position_pattern)) {
-        positionerror.innerText = "name can include alphabates only and characters between 1 to 20";
+        positionerror.innerText = "position name can include alphabates only and characters between 1 to 20";
     } else if (company === "") {
         companyerror.innerText = "enter your company";
     } else if (!company.match(company_pattern)) {
@@ -72,11 +70,10 @@ function validate(e) {
 }
 
 firstname.addEventListener('blur', function () {
-    let firstname = document.querySelector('.firstname').value,
-        firstname_pattern = /^[A-Za-z]{1,20}$/;
+    let firstname = document.querySelector('.firstname').value;
     if (firstname === "") {
         firstnameerror.innerText = "please enter firstname";
-    } else if (!firstname.match(firstname_pattern)) {
+    } else if (!firstname.match(name_pattern)) {
         firstnameerror.innerText = "name can include alphabates only and characters between 1 to 20";
     } else {
         firstnameerror.innerText = "";
@@ -84,11 +81,10 @@ firstname.addEventListener('blur', function () {
 })
 
 lastname.addEventListener('blur', function () {
-    let lastname = document.querySelector('.lastname').value,
-        lastname_pattern = /^[A-Za-z]{1,20}$/;
+    let lastname = document.querySelector('.lastname').value;
     if (lastname === "") {
         lastnameerror.innerText = "please enter lastname";
-    } else if (!lastname.match(lastname_pattern)) {
+    } else if (!lastname.match(name_pattern)) {
         lastnameerror.innerText = "name can include alphabates only and characters between 1 to 20";
     } else {
         lastnameerror.innerText = "";
@@ -96,20 +92,18 @@ lastname.addEventListener('blur', function () {
 })
 
 position.addEventListener('blur', function () {
-    let position = document.querySelector('.position').value,
-        position_pattern = /^[A-Za-z]{1,20}$/;
+    let position = document.querySelector('.position').value;
     if (position === "") {
         positionerror.innerText = "please enter position";
     } else if (!position.match(position_pattern)) {
-        positionerror.innerText = "name can include alphabates only and characters between 1 to 20";
+        positionerror.innerText = "position name can include alphabates only and characters between 1 to 20";
     } else {
         positionerror.innerText = "";
     }
 })
 
 company.addEventListener('blur', function () {
-    let company = document.querySelector('.company').value,
-        company_pattern = /^[A-Za-z0-9]{1,20}$/;
+    let company = document.querySelector('.company').value;
     if (company === "") {
         companyerror.innerText = "enter your company";
     } else if (!company.match(company_pattern)) {
@@ -120,8 +114,7 @@ company.addEventListener('blur', function () {
 })
 
 email.addEventListener('blur', function () {
-    let email = document.querySelector('.email').value,
-        email_pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    let email = document.querySelector('.email').value;
     if (email === "") {
         emailerror.innerText = "enter your email";
     } else if (!email.match(email_pattern)) {
@@ -129,4 +122,8 @@ email.addEventListener('blur', function () {
     } else {
         emailerror.innerText = "";
     }
+})
+
+company_type.addEventListener('focus',function(){
+    alert();
 })
